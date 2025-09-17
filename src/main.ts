2d4a36385b2e9755d8ee7cd04e9939a3ed60dee8
mansii1 <mansimangukiya0@gmail.com>
2025-09-17 23:32:45 +0530

    Added axios call

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import "./style.css";
+import axios from "axios";
+
+const APIURL = "https://api.github.com/users/";
+
+const form = document.getElementById("form") as HTMLFormElement;
+const search = document.getElementById("search") as HTMLFormElement;
+
+async function getUsers(username: String) {
+  try {
+    const { data } = await axios(APIURL + username);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user = search.value;
+  if (user) {
+    getUsers(user);
+    search.value = "";
+  }
+});
